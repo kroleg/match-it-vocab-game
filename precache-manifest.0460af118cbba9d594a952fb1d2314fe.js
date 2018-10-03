e3cdfe799c9b967aeeb4d1d2004f7cebd1aa83e1
Vitaliy Zaytsev <teh.kroleg@gmail.com>
2018-10-03 17:56:34 +0600

    Updates

---
+++ precache-manifest.0460af118cbba9d594a952fb1d2314fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3e3036e04a91476e4d5",
+    "url": "/match-it-vocab-game/static/js/runtime~main.f3e3036e.js"
+  },
+  {
+    "revision": "493c38ba199768a0f09b",
+    "url": "/match-it-vocab-game/static/js/main.493c38ba.chunk.js"
+  },
+  {
+    "revision": "cf93063de07ac318269e",
+    "url": "/match-it-vocab-game/static/js/1.cf93063d.chunk.js"
+  },
+  {
+    "revision": "493c38ba199768a0f09b",
+    "url": "/match-it-vocab-game/static/css/main.70dd3ba0.chunk.css"
+  },
+  {
+    "revision": "c2cd88ebb98807c3a2416aabe1bcb61e",
+    "url": "/match-it-vocab-game/index.html"
+  }
+];
